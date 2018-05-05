b03284ef8f8ed818ce6924ef3c429b21a89989ee
Vijay Pillai <vijay.pillai397@gmail.com>
2018-05-05 17:01:31 -0400

    change to dropdown, got rid of strange script in header

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#!flask/bin/python
+#!/usr/bin/env python
 from flask import Flask, jsonify, make_response, abort, render_template, request
 app = Flask(__name__)
 
@@ -12,20 +12,22 @@ cylinders = {
 }
 # TODO: fix dosage
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/api/v1/medications', methods=['GET'])
 def get_medications():
     return jsonify({'cylinders': cylinders}), 200
 
+
 @app.route('/api/v1/medication', methods=['POST'])
 def create_medication():
-    print "foooo"
     # if not request.json or not 'title' in request.json:
     #     abort(400)
-    cylinders[str(int(request.json['cylinder_number']) % 6)] = { # Mod it by 6
+    cylinders[str(int(request.json['cylinder_number']) % 6)] = {  # Mod it by 6
         # 'id': tasks[-1]['id'] + 1,
         'name': request.json['name'],
         'pills': int(request.json['pills']),
